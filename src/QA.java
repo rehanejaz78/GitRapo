4934d9ffcbb6c558926cbf9744690a12d16db749
rehanejaz78 <82099442+rehanejaz78@users.noreply.github.com>
2022-09-13 14:44:21 +0500

    qa changes

---
+++ src/QA.java
@@ -0,0 +1,11 @@
+public class QA {
+
+    public static void main(String[] args) {
+        System.out.println(" i am in QA branch . this is QA side code");
+
+        int qa = 10;
+        System.out.println(qa);
+
+    }
+
+}
